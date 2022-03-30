4b44d7cbe2f1091447e75a70c99a3fae208e05c4
benduckwdesign <benjamin@benjaminduckworth.com>
2022-03-30 19:35:58 -0400

    fix?

--- chat-view.php
+++ chat-view.php
@@ -10,6 +10,12 @@ require_once 'forms/chat-view.php';
 require_once 'config.php';
 require_once 'db/db.php';
 
+$params = [
+	'siteroot' => $siteroot,
+	'form' => $form,
+	'form_msg' => $form_msg,
+];
+
 $form_msg = "";
 $form_result = "";
 if ($form->isSuccess()) {
@@ -19,18 +25,13 @@ if ($form->isSuccess()) {
         // Load chat if it exists
 		$form = null;
         $chat_messages = $chat_db->get($data->id);
+        $params['chat_messages'] = $chat_messages->messages;
 	} else {
         // Create new chat if it doesn't?
 		$form['id']->addError('That conversation does not exist.');
 	}
 	
 }
-
-$params = [
-	'siteroot' => $siteroot,
-	'form' => $form,
-	'form_msg' => $form_msg,
-];
 
 $latte->render('templates/chat-view.latte', $params);
 
