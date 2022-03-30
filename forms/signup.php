2547d601b2e1191ad205fa61393d223e6eda79f0
benduckwdesign <benjamin@benjaminduckworth.com>
2022-03-30 00:29:24 -0400

    change form layout

--- forms/signup.php
+++ forms/signup.php
@@ -2,9 +2,17 @@
 
 use Nette\Forms\Form;
 $form = new Form;
+$form->addGroup('Registration');
 $form->addText('username', 'Username');
 $form->addPassword('password', 'Password');
 $form->addSubmit('send', 'Sign Up');
 $form->setAction('index.php');
+
+$renderer = $form->getRenderer();
+$renderer->wrappers['controls']['container'] = 'div';
+$renderer->wrappers['pair']['container'] = 'div';
+$renderer->wrappers['pair']['class'] = 'form-pair';
+$renderer->wrappers['label']['container'] = 'div';
+$renderer->wrappers['control']['container'] = 'div';
 
 ?>
