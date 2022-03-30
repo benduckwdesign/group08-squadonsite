f32dad2f7f115df0e0d3ba41e4c8cb89657e2413
benduckwdesign <benjamin@benjaminduckworth.com>
2022-03-29 22:07:15 -0400

    styling forms

--- forms/signup.php
+++ forms/signup.php
@@ -2,9 +2,9 @@
 
 use Nette\Forms\Form;
 $form = new Form;
-$form->addText('username', 'Name:');
-$form->addPassword('password', 'Password:');
-$form->addSubmit('send', 'Sign up');
+$form->addText('username', 'Username');
+$form->addPassword('password', 'Password');
+$form->addSubmit('send', 'Sign Up');
 $form->setAction('index.php');
 
 ?>
