e80e133b6141ba4ede3becd465b2027d667bec7d
benduckwdesign <benjamin@benjaminduckworth.com>
2022-03-31 19:38:00 -0400

    change submit method for forms to GET

--- forms/send-view.php
+++ forms/send-view.php
@@ -10,6 +10,7 @@ $form->addText('message', 'Message')
     ->setRequired('Please write a message.');
 $form->addSubmit('send', 'Send');
 $form->setAction('send-view.php');
+$form->setMethod('GET');
 
 foreach ($form->getControls() as $control) {
 	$control->setOption('class', 'form-'.$control->getOption('type'));
