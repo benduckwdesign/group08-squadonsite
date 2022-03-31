0bb9d07b8772652b04be8f595249e1df5225e68a
benduckwdesign <benjamin@benjaminduckworth.com>
2022-03-31 19:55:10 -0400

    Update send-view.php

--- send-view.php
+++ send-view.php
@@ -17,29 +17,47 @@ $params = [
 $form_msg = "";
 $form_result = "";
 $chat_messages = null;
-if ($form->isSuccess()) {
-    global $params;
-    global $form;
-    global $chat_db;
-	$data = $form->getValues();
-	$chat_exists = $chat_db->has($data->id);
-	if ($chat_exists == True) {
-        global $chat_db;
-        global $params;
-        global $data;
-        global $form;
-        
-	} else {
-        global $form;
-        // Create new chat if it doesn't?
-		$form['message']->addError('That conversation does not exist.');
-	}
-	
-}
 $params['form_msg'] = $form_msg;
 if (isset($_SESSION['chat_id']) == False) {
-    $form['message']->addError('That conversation does not exist.');
+    if ($form->isSuccess()) {
+        global $params;
+        global $form;
+        global $chat_db;
+        $data = $form->getValues();
+        $chat_exists = $chat_db->has($_SESSION['chat_id']);
+        if ($chat_exists == True) {
+            global $chat_db;
+            global $params;
+            global $data;
+            global $form;
+            
+        } else {
+            global $form;
+            // Create new chat if it doesn't?
+            $form['message']->addError('That conversation does not exist.');
+        }
+        
+    }
 } else {
+    if ($form->isSuccess()) {
+        global $params;
+        global $form;
+        global $chat_db;
+        $data = $form->getValues();
+        $chat_exists = $chat_db->has($data->id);
+        if ($chat_exists == True) {
+            global $chat_db;
+            global $params;
+            global $data;
+            global $form;
+            
+        } else {
+            global $form;
+            // Create new chat if it doesn't?
+            $form['message']->addError('That conversation does not exist.');
+        }
+        
+    }
     $params['form'] = $form;
 }
 
