fbb483203ed73d919e4b265ad25f4ce41a8b22c0
benduckwdesign <benjamin@benjaminduckworth.com>
2022-03-31 20:09:02 -0400

    Update chat-view.php

--- chat-view.php
+++ chat-view.php
@@ -14,6 +14,8 @@ $params = [
 	'siteroot' => $ROOTURL
 ];
 
+
+// Create default test chat
 $test_message = new stdClass;
 $test_message->username = "Kirby";
 $test_message->message = "poyo poyopoyo poyo~";
@@ -21,32 +23,34 @@ $test_message->message = "poyo poyopoyo poyo~";
 $test_chat = $chat_db->get('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 $test_chat->messages = array();
 $test_chat->save();
+//
+
 
 $form_msg = "";
 $form_result = "";
 $chat_messages = null;
-if ($form->isSuccess()) {
-    global $params;
-    global $form;
-    global $chat_db;
-	$data = $form->getValues();
-	$chat_exists = $chat_db->has($data->id);
-	if ($chat_exists == True) {
-        global $chat_db;
-        global $params;
-        global $data;
-        global $form;
-        // Load chat if it exists
-		$form = null;
-        $chat_messages = $chat_db->get($data->id)->messages;
-	} else {
-        global $form;
-        // Create new chat if it doesn't?
-		$form['id']->addError('That conversation does not exist.');
-	}
-}
 $params['form_msg'] = $form_msg;
 if (isset($_SESSION['chat_id']) == False) {
+    if ($form->isSuccess()) {
+        global $params;
+        global $form;
+        global $chat_db;
+        $data = $form->getValues();
+        $chat_exists = $chat_db->has($data->id);
+        if ($chat_exists == True) {
+            global $chat_db;
+            global $params;
+            global $data;
+            global $form;
+            // Load chat if it exists
+            $form = null;
+            $chat_messages = $chat_db->get($data->id)->messages;
+        } else {
+            global $form;
+            // Create new chat if it doesn't?
+            $form['id']->addError('That conversation does not exist.');
+        }
+    }
     $params['form'] = $form;
 } else {
     global $params;
@@ -63,6 +67,7 @@ if (isset($_SESSION['chat_id']) == False) {
 		$form = null;
         $chat_messages = $chat_db->get($_SESSION['chat_id'])->messages;
 	}
+    $params['form'] = $form;
 }
 $params['chat_messages'] = $chat_messages;
 
