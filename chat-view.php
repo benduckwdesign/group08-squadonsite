8d330ac772660291f8cf556bb665950f269bfe62
benduckwdesign <benjamin@benjaminduckworth.com>
2022-04-05 23:15:42 -0400

    remove obsolete form entry for chat messages now that session is used

--- chat-view.php
+++ chat-view.php
@@ -8,7 +8,7 @@ $latte = new Latte\Engine;
 
 // $latte->setTempDirectory('cache/');
 
-require_once 'forms/chat-view.php';
+// require_once 'forms/chat-view.php';
 require_once 'config.php';
 require_once 'db/db.php';
 
@@ -34,30 +34,30 @@ if ($chat_db->has('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa') == False) {
 //
 
 
-$form_msg = "";
-$form_result = "";
-$chat_messages = null;
-if ($form->isSuccess()) {
-    global $params;
-    global $form;
-    global $chat_db;
-	$data = $form->getValues();
-	$chat_exists = $chat_db->has($data->id);
-	if ($chat_exists == True) {
-        global $chat_db;
-        global $params;
-        global $data;
-        global $form;
-        // Load chat if it exists
-		$form = null;
-        $chat_messages = $chat_db->get($data->id)->messages;
-	} else {
-        global $form;
-        // Create new chat if it doesn't?
-		$form['id']->addError('That conversation does not exist.');
-	}
-}
-$params['form_msg'] = $form_msg;
+// $form_msg = "";
+// $form_result = "";
+// $chat_messages = null;
+// if ($form->isSuccess()) {
+//     global $params;
+//     global $form;
+//     global $chat_db;
+// 	$data = $form->getValues();
+// 	$chat_exists = $chat_db->has($data->id);
+// 	if ($chat_exists == True) {
+//         global $chat_db;
+//         global $params;
+//         global $data;
+//         global $form;
+//         // Load chat if it exists
+// 		$form = null;
+//         $chat_messages = $chat_db->get($data->id)->messages;
+// 	} else {
+//         global $form;
+//         // Create new chat if it doesn't?
+// 		$form['id']->addError('That conversation does not exist.');
+// 	}
+// }
+// $params['form_msg'] = $form_msg;
 
 if (isset($_SESSION['chat_id']) == False) {
     // pass
@@ -74,7 +74,7 @@ if (isset($_SESSION['chat_id']) == False) {
         $params['chat_messages'] = $chat_db->get($_SESSION['chat_id'])->messages;
 	}
 }
-$params['form'] = $form;
+// $params['form'] = $form;
 $latte->render('templates/chat-view.latte', $params);
 
 ?>
