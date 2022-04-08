d4673e43cd240cc8a0261378a68585299515f02e
benduckwdesign <benjamin@benjaminduckworth.com>
2022-04-08 01:44:44 -0400

    fixed redirect from settings page when not logged in

--- settings-screen.php
+++ settings-screen.php
@@ -2,24 +2,23 @@
 
 require_once 'vendor/autoload.php';
 
-$latte = new Latte\Engine;
-
 // $latte->setTempDirectory('cache/');
 
-require_once 'forms/settings-screen.php';
 require_once 'config.php';
 require_once 'db/db.php';
+
+if (!isset($_SESSION['username'])) { // Redirect if not logged in
+    global $ROOTURL;
+    header("Location: $ROOTURL/login-screen.php");
+    die();
+}
+
+$latte = new Latte\Engine;
+require_once 'forms/settings-screen.php';
 
 $params = [
 	'siteroot' => $ROOTURL
 ];
-
-if (isset($_SESSION['username'])) {
-    // pass
-} else {
-    header("Location: $ROOTURL/login-screen.php");
-    die();
-}
 
 $params['form'] = $form;
 $params['form_msg'] = '';
