ac135d3b0e163895671df03bde6ac670c4aaa13e
benduckwdesign <benjamin@benjaminduckworth.com>
2022-04-15 17:23:15 -0400

    add placeholder screens

---
+++ report-screen.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+require_once 'config.php';
+require_once 'db/db.php';
+
+if (!isset($_SESSION['username'])) { // Redirect if not logged in
+    global $ROOTURL;
+    header("Location: $ROOTURL/login-screen.php");
+    die();
+}
+
+$latte = new Latte\Engine;
+
+// $latte->setTempDirectory('cache/');
+
+$params = [
+	'siteroot' => $ROOTURL
+];
+
+$latte->render('templates/report-screen.latte', $params);
+
+?>
