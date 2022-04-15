cd220cf522d38a4ebb773c412a6b570a621abfac
benduckwdesign <benjamin@benjaminduckworth.com>
2022-04-14 21:50:22 -0400

    Update send-view.php

--- forms/send-view.php
+++ forms/send-view.php
@@ -7,7 +7,8 @@ $form->addText('message', 'Message')
     ->addRule($form::MIN_LENGTH, 'Message must be at least %d characters.', 1)
     ->addRule($form::MAX_LENGTH, 'Message must be at less than %d characters.', 2000)
     ->setHtmlAttribute('placeholder', "Message...")
-    ->setRequired('Please write a message.');
+    ->setRequired('Please write a message.')
+    ->setDefaultValue('');
 $form->addSubmit('send', 'Send');
 $form->setAction('send-view.php');
 
